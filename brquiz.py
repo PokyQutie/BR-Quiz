de05b7228b3aedf252cabc37c9dc64f8046e8efb
PokyQutie <augustinstefan232@gmail.com>
2024-02-25 06:14:57 +0200

    implementat progresie int quiz
    trecut prin quizuri

--- brquiz.py
+++ brquiz.py
@@ -38,9 +38,12 @@ credentials_file = "C:\\Users\\stefi\\Desktop\\br\\BR-Quiz\\br-quiz-415321-45910
 gc = gspread.service_account(filename=credentials_file)
 
 spreadsheet_id = "1LJjuzeEC3tc9mxYmxNyXAn-1-ykkUUS2JiIWdIbBO5s"
+spread2_id = "1d0AlEFfYroWltyLYTI7iEir2S4ND9HGDT9gxwpW6ZW4"
 sheet = gc.open_by_key(spreadsheet_id)
+shit = gc.open_by_key(spread2_id)
 
 worksheet = sheet.sheet1
+workshit = shit.sheet1
 cell = worksheet.cell(row=1, col=7)
 nr_butt = int(cell.value)
 
@@ -51,7 +54,6 @@ for i in range(1,nr_butt+1):
     butoane.append(pula)
 
 for i in butoane:
-    print(i.pozitie)
     import_img(i.url, i.pozitie)
 
 
@@ -63,8 +65,27 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             for i in butoane:
-                if pos[0] >= i.pozitie[0] and pos[1] >= i.pozitie[1] and pos[0] <= i.pozitie[0]+i.lung/2 and pos[1] <= i.pozitie[1]+i.lat:
-                    print("uwu")
+                if pos[0] >= i.pozitie[0] and pos[1] >= i.pozitie[1] and pos[0] <= i.pozitie[0]+i.lung and pos[1] <= i.pozitie[1]+i.lat:
+                    if i.actiune%3 == 0:
+                        import_img("https://i.ibb.co/k0rBN10/fundal.png",(0,0))
+                        nr_int = int(workshit.cell(row=1, col=1).value)
+                        for j in range(1,nr_int+1):
+                            if int(workshit.cell(row=j+1, col=2).value) == i.actiune:
+                                for k in range(1, int(workshit.cell(row=j+1, col=3).value)*2,2):
+                                    import_img(workshit.cell(row=j+1,col=4+k).value,(5,5))
+                                    import_img(workshit.cell(row=j+1,col=4+k+1).value,(5,700-201))
+                                    ok=0
+                                    while ok == 0: #5 201
+                                        for event in pygame.event.get(): 
+                                            if event.type == pygame.MOUSEBUTTONUP:
+                                                pos = pygame.mouse.get_pos()
+                                                if pos[1] >= 700-201:
+                                                    ok=1
+                                    
+                            break
+            import_img("https://i.ibb.co/k0rBN10/fundal.png",(0,0))     
+            for i in butoane:
+                import_img(i.url, i.pozitie)
 
       
         # Check for QUIT event       
